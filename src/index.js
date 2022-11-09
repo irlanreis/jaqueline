db9632de8678c6f5dfdcf109294f3b9927fb2e86
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-08 23:46:49 -0400

    adiciona componentes main e header

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
