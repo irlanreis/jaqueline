7354be678fc7988a14dfec870fb77f191512b8f9
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-11 20:17:57 -0400

    Adiciona atualização

---
+++ src/components/pages/ClienteAlexandro.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ClienteAlexandro extends Component {
+  render() {
+    return (
+      <div>ClienteAlexandro</div>
+    )
+  }
+}
+export default ClienteAlexandro;
+
---
+++ src/components/pages/EspaçoMS.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class EspaçoMS extends Component {
+  render() {
+    return (
+      <div>EspaçoMS</div>
+    )
+  }
+}
+export default EspaçoMS;
